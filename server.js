2606966599f0a7dc878d3291da13aba5c1964a30
al-hpa <althaf.hassan@uts.edu.au>
2024-10-05 23:22:29 +1000

    updated urls in env

--- server.js
+++ server.js
@@ -10,25 +10,37 @@ app.use(express.json());
 
 const seam = new Seam(process.env.SEAM_API_KEY);
 
+let apiUrl;
+
+const server = app.listen(0, () => {
+  const port = server.address().port;
+  const host = process.env.HOST || 'localhost';
+  const protocol = process.env.PROTOCOL || 'http';
+  const localUrl = `${protocol}://${host}:${port}`;
+  apiUrl = process.env.API_URL || localUrl;
+
+  console.log(`Server running at: ${apiUrl}`);
+  console.log(`Get client session token at: ${apiUrl}/get-client-session-token`);
+  console.log(`Get devices at: ${apiUrl}/get-devices`);
+});
+
 app.get('/get-client-session-token', async (req, res) => {
   try {
     const userId = 'single_user_11'; // Replace with actual user ID logic
 
-    // First, try to get an existing session
     let clientSession;
     try {
       clientSession = await seam.clientSessions.get({
         user_identifier_key: userId
       });
     } catch (error) {
-      // If session doesn't exist, create a new one
       if (error.message.includes('not found')) {
         clientSession = await seam.clientSessions.create({
           user_identifier_key: userId,
           expires_in: 3600
         });
       } else {
-        throw error; // Re-throw if it's a different error
+        throw error;
       }
     }
 
@@ -39,22 +51,6 @@ app.get('/get-client-session-token', async (req, res) => {
   }
 });
 
-
-// app.get('/get-client-session-token', async (req, res) => {
-//   try {
-//     const clientSession = await seam.clientSessions.create({
-//       user_identifier_key: 'single_user_11',
-//       expires_in: 3600
-//     });
-//     console.log('Client Session:', JSON.stringify(clientSession, null, 2));
-//     res.json({ clientSessionToken: clientSession.client_session_id });
-//   } catch (error) {
-//     console.error('Error generating client session token:', error);
-//     res.status(500).json({ error: 'Failed to generate client session token' });
-//   }
-// });
-
-// New endpoint to get devices
 app.get('/get-devices', async (req, res) => {
   try {
     const devices = await seam.devices.list();
@@ -63,18 +59,4 @@ app.get('/get-devices', async (req, res) => {
     console.error('Error fetching devices:', error);
     res.status(500).json({ error: 'Failed to fetch devices' });
   }
-})
-
-
-
-const server = app.listen(0, () => {
-  const port = server.address().port;
-  const host = process.env.HOST || 'localhost';
-  const protocol = process.env.PROTOCOL || 'http';
-  const fullUrl = `${protocol}://${host}:${port}`;
-  console.log(`Server running at: ${fullUrl}`);
-  console.log(`Get client session token at: ${fullUrl}/get-client-session-token`);
-  console.log(`Get devices at: ${fullUrl}/get-devices`);
 });
-
-
